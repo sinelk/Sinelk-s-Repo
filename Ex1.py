e2b884b4af45648f025ff1fe123dd0eb5dbece61
Sinelk Berhane <email@sinelk.com>
2020-10-30 22:50:46 -0500

    initial commit

---
+++ Ex1.py
@@ -0,0 +1,8 @@
+Height = int(input("WHAT IS YOUR HEIGHT in cm? "))
+Age = int(input("what is your age? "))
+if Height>=120:
+    print("You can ride the roller coaster!! ")
+elif Age >=18:
+    print("You can ride the roller coaster!! ")
+else:
+    print("You are not tall enough to ride the roller coaster!! ")
