8526540dc4c2e4fdbead76d531987eab3e5b4f13
sinelk <email@sinelk.com>
2020-10-30 22:38:45 -0500

    Update geometry.py

--- geometry.py
+++ geometry.py
@@ -1,1 +1,13 @@
-"# Sinelk-s-Repo" 
+from tkinter import *
+
+def Call():
+    msg = Label(window, text = "Don't press the button again!")
+    msg.place(x = 30, y = 50)
+    button["bg"] = "blue"
+    button["fg"] = "white"
+
+window = Tk()
+window.geometry("200x100")
+button = Button(text = "press me", command = Call)
+button.place(x = 30, y = 20, width=120, height=25)
+window.mainloop()
